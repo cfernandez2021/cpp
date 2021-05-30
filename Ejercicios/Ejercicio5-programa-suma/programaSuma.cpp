f03192a9ca40822a0fe3d88655bccc939a713f06
carlos fernandez <cfernandezd@unah.hn>
2021-05-30 16:14:05 -0600

    agregando ejercicio5 programa suma

---
+++ Ejercicios/Ejercicio5-programa-suma/programaSuma.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+//Entrada//
+int a=0;
+int b=0;
+int resultado;
+	
+	cout<<"Ingrese el valor de A:"<<endl;
+	cin>>a;
+	
+	cout<<"Ingrese el valor de B"<<endl;
+	cin>>b;
+	
+	
+		  //proceso//
+resultado=a+b;
+
+//Salida//
+cout<<"La suma de a+b es: "<<resultado<<endl;
+
+
+	return 0;
+}

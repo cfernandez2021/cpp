1b1412178ccf7430a3b50233bcfbfe165317b9cb
carlos fernandez <cfernandezd@unah.hn>
2021-05-30 15:54:01 -0600

    agregando ejercicio4 y 5

---
+++ Ejercicios/Ejercicio3-Variables/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero=15;
+    cout<<numeroEntero<<endl;
+
+    numeroEntero=18;
+
+    cout<<numeroEntero<<endl;
+
+    return 0;
+}
+
+
---
+++ Ejercicios/Ejercicio4-Imprimiendo Valores de todas las variables/todas-las-variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    bool ValorBoolean;
+    ValorBoolean=false;
+    int ValorEntero=15;
+    double ValorDouble=20.99;
+    string ValorString="Hola como estan";
+    char ValorChar='B';
+
+    cout<<"Valor Booolean:"<<ValorBoolean<<endl;
+     cout<<"Valor Entero:"<<ValorEntero<<endl;
+    cout<<"Valor Double:"<<ValorDouble<<endl;
+    cout<<"Valor String:"<<ValorString<<endl;
+    cout<<"Valor Char:"<<ValorChar<<endl;
+    
+
+    return 0;
+}

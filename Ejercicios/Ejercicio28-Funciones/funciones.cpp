1ea124645a39294c96bcb4ccc435ebccf1c0d362
carlos fernandez <cfernandezd@unah.hn>
2021-06-08 20:08:39 -0600

    subiendo ejercicio 28 y 29

---
+++ Ejercicios/Ejercicio28-Funciones/funciones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sumar()
+{
+    return 5+7;
+}
+int main(int argc, char const *argv[])
+{
+    int resultado=sumar();
+    cout<<"El resultado de esta funcion es: "<<resultado<<endl;
+    return 0;
+
+
+}
+
+
+
---
+++ Ejercicios/Ejercicio29-funciones int/funcionint.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//funcion sumar//
+int sumar(int a , int b)
+{
+    return a+b;
+}
+
+//main
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    cout<<sumar(7,5);
+    cout<<endl;
+
+    
+    return 0;
+}

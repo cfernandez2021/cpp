019580ce038b04c57221226d5e339b2801cdfeb2
carlos fernandez <cfernandezd@unah.hn>
2021-06-08 19:48:25 -0600

    subiendo ejercicio25

---
+++ Ejercicios/Ejercicio25-Suma de pares  e impares/pares.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    
+    int pares=0;
+    int impares=0;
+    int total=0;
+    
+    cout<<"pares"<<endl;
+    for(int i=1;i<=10;i++)
+    {
+        if(i%2==0)
+        {
+            pares=pares+i;
+
+        }
+
+        cout<<i<<" ";
+    }
+    cout<<endl;
+      cout<<endl;
+
+      cout<<"impares"<<endl;
+     for(int i=1;i<=10;i++)
+    {
+        if(i%2==1)
+        {
+            impares=impares+i;
+
+        }
+
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    total=pares+impares;
+    cout<<"Total pares: "<<pares<<endl;
+    cout<<"Total impares: "<<impares<<endl;
+    cout<<"El total de pares e impares es: "<<total<<endl;
+
+
+    return 0;
+}

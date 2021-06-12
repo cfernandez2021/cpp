8e12fa9ba854f8cb63f6ef90423ffab6bc3bbd34
carlos fernandez <cfernandezd@unah.hn>
2021-06-11 20:55:10 -0600

    agregando ejercicio 31

---
+++ Ejercicios/Ejercicio31-calculadora-funciones/calculador.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a,int b ){
+    return a+b;
+}
+
+int calculadora(int a,int b , char operador){
+    if (operador== '+'){
+        return sumar(a,b);
+    }
+
+    return -1;
+}
+
+int main(int argc, char const *argv[])
+{
+    system("cls");   
+    int num1=0;
+    int num2=0;
+    char operador;
+
+    cout<<"Digite el valor de a: ";
+    cin>>num1;
+
+    cout<<"Digite el valor de b: ";
+    cin>>num2;
+
+    cout<<"ingrese el operador (+,-,*,/)";
+    cin>>operador;
+
+
+    cout<<"El resultado de "<<num1<<" "<<operador<<" "<<num2<<" es "<<sumar(num1,num2);
+
+
+    return 0;
+}
